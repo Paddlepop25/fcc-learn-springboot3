3e18052ebb9a6e90f9163d3fd2ffe7c0e6cfd831
Paddlepop25 <shecodeshsucodes18@gmail.com>
2024-11-22 13:47:02 +0800

    added logger, like console.log

--- src/main/java/dev/linda/runnerz/RunnerzApplication.java
+++ src/main/java/dev/linda/runnerz/RunnerzApplication.java
@@ -1,11 +1,18 @@
 package dev.linda.runnerz;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class RunnerzApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(RunnerzApplication.class);
+
+	// the main method is the entry point for this application
 	public static void main(String[] args) {
 		SpringApplication.run(RunnerzApplication.class, args);
 
@@ -14,5 +21,8 @@ public class RunnerzApplication {
 
 		// a bean is an instance of a class with some metadata attached to it
 
+		// we can add logs, like console.log
+		// different types of log, like log.error, log.warn, etc
+		log.info(">>> Application started successfully <<<");
 	}
 }
